c040d193e07991b9390b6a7ef43528d002a4c512
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-09-03 12:52:11 -0700

    cards

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
-            filteredUsers: [],
             query: "world"
 
         }
@@ -21,13 +19,13 @@ class App extends Component {
 
 
     }
-
+    // https://api.unsplash.com/search/collections?page=1&query=office
 
 
 
     componentDidMount() {
         request
-            .get(`https://api.unsplash.com/photos?client_id=b32091e8b92b6bb1f30f2409ef141dc0c31ffc30cc7dfa5ad46308738fe4c1f0&query=${this.state.query}&per_page=100`).then(res => {
+            .get(`https://api.unsplash.com/photos/?client_id=b32091e8b92b6bb1f30f2409ef141dc0c31ffc30cc7dfa5ad46308738fe4c1f0&query=${this.state.query}&per_page=30`).then(res => {
                 if (res.ok) {
                     console.log(res.body)
             
